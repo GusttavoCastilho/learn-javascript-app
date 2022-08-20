f030b735db582ad97404a12b36d3766d4a0906d2
GusttavoCastilho <gustavocvalenciano@gmail.com>
2022-08-20 16:55:36 -0400

    chore: configure alias path

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@navigation": "./src/presentation/navigation",
+            "@components": "./src/presentation/components",
+            "@screens": "./src/presentation/screens",
+            "@assets": "./assets",
+          },
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          root: ["./src"],
+        },
+      ],
+    ],
   };
 };
