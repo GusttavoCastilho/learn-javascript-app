08fedbeecc81fe56eebe28735c8766a9eb92bdf1
GusttavoCastilho <gustavocvalenciano@gmail.com>
2022-08-22 19:27:30 -0400

    style: update theme

--- src/presentation/styles/theme.ts
+++ src/presentation/styles/theme.ts
@@ -3,6 +3,11 @@ export const theme = {
 		primary: '#40F99B',
 		secondary: '#E85D75',
 		tertiary: '#9D69A3',
-		white: '#FFFFFF'
+		white: '#FFFFFF',
+		bg: '#F5F5F5',
+		dark: {
+			100: '#222'
+		},
+		red: '#ff0000'
 	}
 } as const
