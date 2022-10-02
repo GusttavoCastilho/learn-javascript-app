642dc246845a61d6a7182f5bb11aaa1e4888de45
GusttavoCastilho <gustavocvalenciano@gmail.com>
2022-10-02 11:26:09 -0400

    feat: install and configure safeAreaProvider

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "@styles/theme";
 import { Navigation } from "./navigation";
-import { AuthProvider } from '../presentation/context/auth/auth-context'
+import { AuthProvider } from "../presentation/context/auth/auth-context";
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <AuthProvider>
-      <Navigation />
-      </AuthProvider>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <Navigation />
+        </AuthProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
